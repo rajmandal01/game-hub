9191c2d921bc9f0927a4016aa97e399c0d57a22d
rajmandal800 <rajmandal800@gmail.com>
2025-09-24 12:26:22 +0200

    added optimistic updates in todos

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -7,27 +7,53 @@ import axios from "axios";
 export default function TodoForm() {
   const ref = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
+  interface AddTodoContext {
+    previousTodos: Todo[];
+  }
 
-  const addTodo = useMutation<Todo, Error, Todo>({
+  /**
+   * Optimistic update
+   * 1.
+   *
+   */
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     // useMutation <ServerResponse Type, Error type, Mutation/Post data type>
     mutationFn: (todo: Todo) =>
       axios
-        .post("https://jsonplaceholder.typicode.com/todos", todo)
+        .post("https://jsonplaceholder.typicode.com/todosx", todo)
         .then((respose) => respose.data),
-    onSuccess: (savedTodo, todo) => {
+
+    onMutate: (newTodo: Todo) => {
+      // Updates the cache before sending data to server
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [newTodo, ...(todos || [])]);
+      if (ref.current) ref.current.value = "";
+
+      return { previousTodos }; // return old data in context such that incase of error in new data add/update this can be set to cache, basically rollback!
+    },
+    onSuccess: (savedTodo, newTodo) => {
       // APPROACH 1: Invalidating the cache, React Query then automatically fetch todos
       //   queryClient.invalidateQueries({ queryKey: ["todos"] });
-
       // APPROACH 2: Updating the data in cache
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [savedTodo, ...(todos || [])]);
-      if (ref.current) ref.current.value = "";
+      // queryClient.setQueryData<Todo[]>(["todos"], (todos) => [savedTodo, ...(todos || [])]);
+      //  if (ref.current) ref.current.value = "";
+
+      // Replacing the todo returned from server with the dummy one added for optimistic udpate
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error: Error, newTodo, context) => {
+      if (!context) return;
+      // Rollback
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
     },
   });
 
   return (
     <>
       {addTodo.error && <div>{addTodo.error.message}</div>}
-      <form>
+      <form onSubmit={(e) => e.preventDefault()}>
         <InputGroup>
           <Input ref={ref} borderRadius={20} placeholder="Add Todo..." variant="filled" />
           <Button
