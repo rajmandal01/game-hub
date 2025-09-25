758eec272b29a4a976189efd6cc57b3f1380d436
rajmandal800 <rajmandal800@gmail.com>
2025-09-25 11:22:50 +0200

    Implement infinte queries

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,11 @@ export interface GameQuery {
   platform: Platform | null;
   sortOrder: string;
   searchText: string;
+  pageSize: number;
 }
 
 function App() {
-  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  const [gameQuery, setGameQuery] = useState<GameQuery>({ pageSize: 12 } as GameQuery);
 
   return (
     <Grid
